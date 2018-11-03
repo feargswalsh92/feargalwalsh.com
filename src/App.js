421c55185bf118b91d63339a1339f190ec2447e7
feargswalsh92 <feargswalsh@gmail.com>
2018-11-03 11:05:51 -0700

    (sometimes)

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <ul>
         <li class="building">Currently working at <a href="https://www.obo.pm/" target="_blank">Obo</a></li>
         <li class="studied">Studied at <a href="https://www.luc.edu/" target="_blank">Loyola</a> Masters CS </li>
-        <li class="lives">Lives in <a href="https://goo.gl/maps/hWSw1iW4oT42" target="_blank">San Mateo</a>, works out of <a href="https://goo.gl/maps/6XYusnNUhK82" target="_blank">the local library sometimes</a></li>
+        <li class="lives">Lives in <a href="https://goo.gl/maps/hWSw1iW4oT42" target="_blank">San Mateo</a>, works out of <a href="https://goo.gl/maps/6XYusnNUhK82" target="_blank">the local library(sometimes)</a></li>
         <li class="made">Made in <a href="https://goo.gl/maps/DP63Mgr9iTn" target="_blank">Ireland</a></li>
         <li class="loves">Loves <a href="http://www.ycombinator.com/" target="_blank">YC, </a>Eternal Sunshine of the Spotless Mind, Kanye and Yoga.</li>
         <li class="on">On <a href="https://github.com/feargswalsh92" target="_blank">Github</a>, <a href="https://medium.com/@feargswalsh" target="_blank">Medium</a></li>
