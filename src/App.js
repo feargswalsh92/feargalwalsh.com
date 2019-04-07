60aefc3f64cc18dcdff4a59c50f2fa1505425d21
feargswalsh92 <feargswalsh@gmail.com>
2019-04-07 15:32:02 -0700

    fix commented out componentwillmount

--- src/App.js
+++ src/App.js
@@ -2,31 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-componentWillMount() {
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.onload = () => {
-      this.setState({
-          isLoaded: true
-      });
-  };
-  script.src = 
-  function(w,d,v3){
-    w.chaportConfig = { appId : '5c984a0d8102813e60ae8741' };
-    if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};
-    var s=d.createElement('script');s.type='text/javascript';s.async=true;
-    s.src='https://app.chaport.com/javascripts/insert.js';
-    var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)}(window, document);
-    document.head.appendChild(script);
-}
+// componentWillMount() {
+//   const script = document.createElement('script');
+//   script.type = 'text/javascript';
+//   script.onload = () => {
+//       this.setState({
+//           isLoaded: true
+//       });
+//   };
+//   script.src = 
+//   function(w,d,v3){
+//     w.chaportConfig = { appId : '5c984a0d8102813e60ae8741' };
+//     if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};
+//     var s=d.createElement('script');s.type='text/javascript';s.async=true;
+//     s.src='https://app.chaport.com/javascripts/insert.js';
+//     var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)}(window, document);
+//     document.head.appendChild(script);
+// }
 
-getInitialState() {
-  return {
-      isLoaded: false
-  }
-}
+// getInitialState() {
+//   return {
+//       isLoaded: false
+//   }
+// }
 
 class App extends Component {
+  state = { isLoaded: true };
   render() {
     return (  <div id="body">
     <div id="content">
