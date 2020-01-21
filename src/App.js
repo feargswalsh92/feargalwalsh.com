4b38c00e4d073bcf4906c184867769f762f5bf55
feargswalsh92 <feargswalsh@gmail.com>
2020-01-21 10:55:27 -0800

    resume and copy update; babel updates

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <li class="studied">studied at <a href="https://www.luc.edu/" target="_blank">Loyola</a> Masters CS </li>
         <li class="lives">lives in <a href="https://goo.gl/maps/hWSw1iW4oT42" target="_blank">San Francisco</a></li>
         <li class="made">made in <a href="https://goo.gl/maps/DP63Mgr9iTn" target="_blank">Ireland</a></li>
-        <li class="loves">loves <a href="http://www.ycombinator.com/" target="_blank">YC, </a> Kanye and Yoga.</li>
+        <li class="loves">loves <a href="http://www.ycombinator.com/" target="_blank">YC, </a> Yoga and reading</li>
         <li class="on">on <a href="https://github.com/feargswalsh92" target="_blank">Github </a>and <a href="https://medium.com/@feargswalsh" target="_blank">Medium.</a></li>
         <li class="resume">you can download my resume in <a href = {resumeWord} target = "_blank"> word </a> or <a href = {resumePDF} target = "_blank"> pdf. </a></li>
       </ul>
